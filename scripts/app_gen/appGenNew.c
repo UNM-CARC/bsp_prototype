47e45d8d3aa8ed915e63d8b03bedadd7a3420c68
Quincy Wofford <quincy.wofford@gmail.com>
2019-03-25 12:45:57 -0600

    updating collection data

--- scripts/app_gen/appGenNew.c
+++ scripts/app_gen/appGenNew.c
@@ -40,6 +40,7 @@
 
 struct coll_time{
   int rank;
+  unsigned long expected_sleep;
   unsigned long start;
   unsigned long end;
   unsigned long sleep;
@@ -99,6 +100,7 @@ barrier_loop(double a, double b, char * distribution, int iterations, struct col
   //FILE *f;
   //f = fopen("result_coll", "a");
 
+  double coll_exp_sleep = 0.0;
   double coll_start = 0.0;
   double coll_end = 0.0;
   double coll_sleep = 0.0;
@@ -248,6 +250,7 @@ barrier_loop(double a, double b, char * distribution, int iterations, struct col
 // SLEEP START TODO
 #ifdef USE_METRICS
     coll_sleep = MPI_Wtime();
+    coll_exp_sleep = inter_time;
 #endif
     if (inter_time > 0){
 
@@ -276,6 +279,7 @@ barrier_loop(double a, double b, char * distribution, int iterations, struct col
     times_buffer[ i ].rank = rank;
     times_buffer[ i ].start = (unsigned long) coll_start;
     times_buffer[ i ].end =  (unsigned long) coll_end;
+    times_buffer[ i ].expected_sleep = (unsigned long) coll_exp_sleep;
     times_buffer[ i ].sleep = (unsigned long) coll_sleep;
 #endif
   }
@@ -310,9 +314,9 @@ barrier_loop(double a, double b, char * distribution, int iterations, struct col
 
 
 #ifdef USE_METRICS
-    app_end_time   = 1000000000 * MPI_Wtime();
-    app_start_time = 1000000000 * app_start_time;
-    unsigned long final = (unsigned long) (app_end_time - app_start_time);
+  app_end_time   = 1000000000 * MPI_Wtime();
+  app_start_time = 1000000000 * app_start_time;
+  unsigned long final = (unsigned long) (app_end_time - app_start_time);
   for (i = 0; i < iterations; i++) {
 
     fprintf(f_time, "%s,", experimentID);
@@ -322,10 +326,12 @@ barrier_loop(double a, double b, char * distribution, int iterations, struct col
     fprintf(f_time, "%f,", a);
     fprintf(f_time, "%f,", b);
     fprintf(f_time, "%u,", iterations);
-    fprintf(f_time, "%lu,%lu,%lu",
-        times_buffer[i].sleep    ,
-        times_buffer[i].start    ,
-        times_buffer[i].end      );
+    fprintf(f_time, "%lu,%lu,%lu,%lu,%lu",
+        times_buffer[i].sleep         ,
+        times_buffer[i].start         ,
+        times_buffer[i].end           ,
+        times_buffer[i].expected_sleep,
+        times_buffer[i].start - times_buffer[i].sleep);
     fprintf(f_time, "\n");
   }
   fprintf(f_time, "\n");
