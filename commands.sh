0aaaaae1115e770fb3aefc34ae1a4fc31148c4d0
Patrick G. Bridges <patrickb@unm.edu>
2019-07-26 14:58:24 -0600

    Setting up shell scriptto be able to pass a filename to app_gen_metrics (arg still needed)

--- commands.sh
+++ commands.sh
@@ -3,5 +3,7 @@
 #module load mpi/openmpi3-x86_64
 mkdir -p /tmp/results/
 echo "Running with commands $@"
-/opt/bsp_prototype/scripts/app_gen/app_gen_metrics "$@"
-cp /tmp/results/* /results/
+TMPFILE=`mktemp -d /tmp/results/{PBS_JOBNAME}-${PBS_O_HOST}-${PBS_ARRAYID}.XXXXXX`
+/opt/bsp_prototype/scripts/app_gen/app_gen_metrics -f ${TMPFILE} "$@"
+cp ${TMPFILE} /results/
+rm ${TMPFILE}
