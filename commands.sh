e16ce8b7d96cc907587e3b0d391dc0ba5f318610
Patrick G. Bridges <patrickb@unm.edu>
2019-07-26 13:04:25 -0600

    Update commands

--- commands.sh
+++ commands.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-scl enable rh-python36 bash
-module load mpi/openmpi3-x86_64
+#scl enable rh-python36 bash
+#module load mpi/openmpi3-x86_64
 mkdir -p /tmp/results/
 echo "Running with commands $@"
 /opt/bsp_prototype/scripts/app_gen/app_gen_metrics "$@"
