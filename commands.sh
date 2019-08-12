f59d8c5c42a02b07f07f6fe0c04c5169e5cfec02
Patrick Bridges <patrickb@unm.edu>
2019-08-12 16:12:23 -0600

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -3,7 +3,7 @@
 #module load mpi/openmpi3-x86_64
 mkdir -p /tmp/results/
 echo "Running with commands $@"
-TMPFILE=`mktemp /tmp/results/${PBS_JOBNAME}-${PBS_O_HOST}-${PBS_ARRAYID}.XXXXXX`
+TMPFILE=`mktemp /tmp/results/${PBS_JOBNAME}-${PBS_O_HOST}.XXXXXX`
 /opt/bsp_prototype/scripts/app_gen/app_gen_metrics -f ${TMPFILE} "$@"
 cp ${TMPFILE} /results/
 rm ${TMPFILE}
