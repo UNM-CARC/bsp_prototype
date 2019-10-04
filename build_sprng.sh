8d38d6c90f6da7246c3bfe89f4d837b4bf31073e
Patrick G. Bridges <patrickb@unm.edu>
2019-10-04 16:24:32 -0600

    Missed file added

--- build_sprng.sh
+++ build_sprng.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -l
-module load openmpi-3.1.4-gcc-4.8.5-2e57mrc
-configure --with-mpi --enable-debug && make && make install
+module load openmpi-3.1.4-gcc-4.8.5-chobv7u
+./configure --with-mpi --with-fortran=no --prefix=/usr && make install
