ca5cdbab19b58d9842fd08566f8ae31cddc5b524
Patrick G. Bridges <patrickb@unm.edu>
2019-10-04 21:27:47 -0600

    More updates to make the cleaned Docker container build

--- Dockerfile
+++ Dockerfile
@@ -5,13 +5,15 @@ FROM unmcarc/docker_base:spack
 # mkdir -p /opt/bsp_prototype
 #RUN wget -P /opt/bsp_prototype/ https://github.com/UNM-CARC/bsp_prototype/archive/a0a3e9b5f2c31f5cbccce6c1e1b618ea98152d62.zip
 #RUN unzip /opt/bsp_prototype/a0a3e9b5f2c31f5cbccce6c1e1b618ea98152d62.zip
-COPY sprng5 /root
+COPY sprng5/ /root/sprng5/
 WORKDIR /root/sprng5
-COPY build_sprng.sh .
-RUN build_sprng.sh 
+RUN module load openmpi-3.1.4-gcc-4.8.5-chobv7u && ./configure --with-mpi --with-fortran=no --prefix=/usr && make install
+COPY build_sprng.sh /root/sprng5/build_sprng.sh
+RUN ["chmod", "+x", "/root/sprng5/build_sprng.sh"]
+RUN /root/sprng5/build_sprng.sh
 WORKDIR /root
-COPY Makefile bsp_prototype.c gsl-sprng.h /root
-RUN make
+COPY Makefile bsp_prototype.c gsl-sprng.h /root/
+RUN module load openmpi-3.1.4-gcc-4.8.5-chobv7u && make bsp_prototype
 COPY commands.sh /root/commands.sh
 RUN ["chmod", "+x", "/root/commands.sh"]
 CMD ["/root/commands.sh"] 
--- bsp_prototype.c
+++ bsp_prototype.c
@@ -1,6 +1,7 @@
 /*
- * Copyright(C) 2015. Oscar Mondragon
- * appGen.c
+ * Copyright 2019. Keira Haskins and Patrick G. Bridges, University of New Mexico
+ * Copyright 2015. Oscar Mondragon
+ * bsp_prototype.c (previously appGen.c)
  * Description:
  *
  */
@@ -16,10 +17,10 @@
 #include <openssl/ssl.h>
 
 #include <gsl/gsl_rng.h>
+#include "gsl-sprng.h"
 #include <gsl/gsl_randist.h>
 
-//#include "gsl-sprng.h"
-#include "sprng_cpp.h"
+//#include "sprng_cpp.h"
 
 #include <assert.h>
 
@@ -27,12 +28,10 @@
 
 #include <time.h>
 
-
-
 /*
  * If distribution is:
  * exponential,    a = interarrival mean
- * gaussian,     a = interarrival mean, b = interarrival sttdev
+ * gaussian,     a = interarrival mean, b = interarrival stddev
  * flat,     a = start number, b = end number
  * pareto,    a = exponent (i.e., shape)
  *      b = scale (shift to the right ~ largest value in tail),
@@ -48,6 +47,10 @@ struct coll_time{
 };
 
 
+/* 
+ * Code to sleep for a pre-determined about of time on an x86_64 system as
+ * closely as possible by busy-waiting on the time stamp counter 
+ */
 unsigned long long rdtsc(void)
 {
   unsigned int lo, hi;
@@ -60,7 +63,7 @@ unsigned long long rdtsc(void)
  * http://sci.tuomastonteri.fi/programming/cplus/x86timer
  */
 
-double//uint64_t
+double
 get_clocks_per_nanosecond() {
 
   int sum=0;
@@ -91,14 +94,75 @@ void sleep_rdtsc(uint64_t nanoseconds, double clocks_per_nanosecond)
   }while ( (now-begin) < dtime);
 }
 
+
+/* Code to generate the length of the random intervals on each node */
+enum rng_type {
+	RNG_ERROR=-1,
+	RNG_GAUSSIAN,
+	RNG_EXPONENTIAL,
+	RNG_FLAT,
+	RNG_PARETO,
+	RNG_CONSTANT
+};
+
+enum rng_type init_rng_type(char *distribution)
+{
+    	if (strcmp(distribution,"gaussian") == 0){
+		return RNG_GAUSSIAN;
+    	} else if (strcmp(distribution,"exponential") == 0){
+		return RNG_EXPONENTIAL;
+    	} else if (strcmp(distribution,"flat") == 0){
+		return RNG_FLAT;
+    	} else if (strcmp(distribution,"pareto") == 0){
+		return RNG_PARETO;
+    	} else if (strcmp(distribution, "constant") == 0)
+		return RNG_CONSTANT;
+	else return RNG_ERROR;
+}
+
+double generate_interval_rng(gsl_rng *r, enum rng_type rng_type, double a, double b) 
+{
+	double inter_time;
+	double inter_mean, inter_stddev, exponent, scale; 
+	do {
+		switch (rng_type) {
+		case RNG_GAUSSIAN:
+      	    		inter_mean = a;
+			inter_stddev = b;
+
+        		inter_time =  gsl_ran_gaussian (r, inter_stddev) + inter_mean;
+			break;
+    		case RNG_EXPONENTIAL:
+        		inter_mean = a;
+        		inter_time =  gsl_ran_exponential (r, inter_mean);
+			break;
+    		case RNG_FLAT:
+        		inter_time = gsl_ran_flat (r,a,b);
+			break;
+		case RNG_PARETO:	
+      			exponent = a;
+      			scale = b;
+      			inter_time = gsl_ran_pareto (r,a,b);
+			break;
+		/* Note the straight return out in the next two to avoid the
+ 		 * while loop if a is 0.0 */
+		case RNG_CONSTANT:
+			return a;
+		default:
+			return 0.0;
+		}
+	} while (inter_time == 0.0);
+	return inter_time;
+}
+/* 
+ * Main loop for the program - sleep in a barrier some number of iterations
+ * with the length of each iteration drawn from a random distribution. 
+ * Log the desired local, actual local, and actual global sleep times
+ * as we run for later output.
+ */
 int
 barrier_loop(double a, double b, char * distribution, int iterations, struct coll_time * times_buffer, double cpn){
 
-
-#ifdef USE_METRICS
-
-  //FILE *f;
-  //f = fopen("result_coll", "a");
 
   double coll_exp_sleep = 0.0;
   double coll_start = 0.0;
@@ -108,46 +172,7 @@ barrier_loop(double a, double b, char * distribution, int iterations, struct col
   double rank_start_time = 0.0;
   double rank_end_time = 0.0;
 
-#endif
-  double app_start_time = 0.0;
-  double app_end_time = 0.0;
-  time_t rawtime;
-  struct tm * timeinfo;
-  struct stat st = {0};
-
-  FILE *f_time;
-  int i, length = 13;
-  int root = 0;
-  const char *str = "0123456789abcdef";
-  char tmpfile[512];
-  char tmpbuff[64];
-  char hostname[1024];
-  char out[20];
-  char experimentID[20];
-
-  hostname[1023] = '\0';
-  gethostname(hostname, 1023);
-  // Create new directories in tmp: /tmp/results/<DISTRIBUTION> to
-  // store results before writing back to the file system.
-  strcpy(tmpfile, "/tmp/results");
-  if (stat(tmpfile, &st) == -1) {
-    mkdir(tmpfile, 0700);
-  }
-  strcat(tmpfile, "/");
-  strcat(tmpfile, hostname);
-  strcat(tmpfile, "_");
-
-  srand((unsigned int) time(0) + getpid());
-
-  for (i = 0; i < length; i++) {
-    out[i] = str[(rand() % 16)];
-    srand(rand());
-  }
-  out[i] = 0;
-  strcat(tmpfile, out);
-  strcat(tmpfile, ".csv");
-
-  f_time = fopen(tmpfile, "a");
+  int i;
   double inter_time = 0;
   int rank = 0;
   int nprocs = 0;
@@ -162,115 +187,31 @@ barrier_loop(double a, double b, char * distribution, int iterations, struct col
    *  SPRNG adds to GSL the capacity to generate independent streams of random variables across MPI ranks
    */
 
-  r = gsl_rng_alloc (gsl_rng_ran2);
-
+  r = gsl_rng_alloc (gsl_rng_sprng20);
+  enum rng_type rng_type = init_rng_type(distribution);
+  if (rng_type < 0) {
+      printf("Please provide a valid distribution type\n");
+      return -1;
+  }
   double resolution = MPI_Wtick();
 
-#ifdef USE_METRICS
   rank_start_time = MPI_Wtime();
-#endif
-
-  if (rank == 0)
-    app_start_time = MPI_Wtime();
-
-
-  //printf("Time resolution %f\n", resolution);
-  //printf("Setting start time %f\n", rank_start_time);
 
   for( i = 0; i < iterations; i++){
+    inter_time = generate_interval_rng(r, rng_type, a, b);
+    
+    assert(inter_time >= 0.0 );
 
-    if (strcmp(distribution,"gaussian") == 0){
-
-      double inter_mean = a;
-      double inter_sttdev = b;
-
-      inter_time =  gsl_ran_gaussian (r, inter_sttdev) + inter_mean;
-      // Avoid inter arrival time zero
-      while(inter_time == 0)
-        inter_time =  gsl_ran_gaussian (r, inter_sttdev) + inter_mean;
-    }
-
-    else if (strcmp(distribution,"exponential") == 0){
-
-      double inter_mean = a;
-
-      inter_time =  gsl_ran_exponential (r, inter_mean);
-      // Avoid inter arrival time zero
-      while(inter_time == 0)
-        inter_time =  gsl_ran_exponential (r, inter_mean);
-    }
-
-
-    else if (strcmp(distribution,"flat") == 0){
-
-      double start = a;
-      double end = b;
-
-      inter_time = gsl_ran_flat (r,a,b);
-
-      // Avoid inter arrival time zero
-      while(inter_time == 0)
-        inter_time = gsl_ran_flat (r,a,b);
-
-    }
-
-    else if (strcmp(distribution,"pareto") == 0){
-
-      double exponent = a;
-      double scale = b;
-
-      inter_time = gsl_ran_pareto (r,a,b);
-
-      // Avoid inter arrival time zero
-      while(inter_time == 0)
-        inter_time = gsl_ran_pareto (r,a,b);
-
-    }
-
-    else if (strcmp(distribution,"constant") == 0){
-
-      inter_time = a;
-
-    }
-
-
-    else{
-      printf("Please provide a valid distribution\n");
-      return -1;
-    }
-
-    //printf("(rank %d, it %d) inter_time %f\n", rank, i, inter_time);
-
-    assert(inter_time >= 0 );
-
-    /* If computation time < 1ms use
-     * rdtsc + busy-waiting delay approach
-     * otherwise use usleep()
-     */
-#ifdef USE_METRICS
     coll_sleep = MPI_Wtime();
     coll_exp_sleep = inter_time;
-#endif
     if (inter_time > 0){
-
-      //if (inter_time < 1000)
         sleep_rdtsc(1000  * inter_time, cpn);
-      //else
-      //  usleep(inter_time);
     }
 
-#ifdef USE_METRICS
     coll_start = MPI_Wtime();
-#endif
     MPI_Barrier(MPI_COMM_WORLD);
-
-#ifdef USE_METRICS
     coll_end = MPI_Wtime();
-#endif
 
-    //printf("rank %d MPI_Barrier %lu %lu\n", rank, (unsigned long)coll_start, (unsigned long)coll_end );
-
-#ifdef USE_METRICS
     coll_start =     ( coll_start - rank_start_time ) * 1000000000;
     coll_end   =     ( coll_end   - rank_start_time ) * 1000000000;
     coll_sleep =     ( coll_sleep - rank_start_time ) * 1000000000;
@@ -281,111 +222,112 @@ barrier_loop(double a, double b, char * distribution, int iterations, struct col
     times_buffer[ i ].end =  (unsigned long) coll_end;
     times_buffer[ i ].expected_sleep = (unsigned long) coll_exp_sleep;
     times_buffer[ i ].sleep = (unsigned long) coll_sleep;
-#endif
   }
-/*
-#ifdef USE_METRICS
-  for (i = 0; i < iterations;  i++ ){
-
-    //printf("iterations %d\n",i);
-
-    fprintf(f, "rank %d MPI_Barrier %lu %lu\n",
-    times_buffer[ i ].rank,
-    times_buffer[ i ].start,
-    times_buffer[ i ].end);
-  }
-#endif
-*/
-
-  if (rank == 0) {
-    srand((unsigned int) time(0) + getpid());
-    for (i = 0; i < length; i++) {
-      experimentID[i] = str[(rand() % 16)];
-      srand(rand());
-    }
-    experimentID[i] = 0;
-  }
-  MPI_Bcast(&experimentID,
-     sizeof(experimentID),
-                 MPI_CHAR,
-                     root,
-           MPI_COMM_WORLD);
-
-
-
-#ifdef USE_METRICS
-  app_end_time   = 1000000000 * MPI_Wtime();
-  app_start_time = 1000000000 * app_start_time;
-  unsigned long final = (unsigned long) (app_end_time - app_start_time);
-  for (i = 0; i < iterations; i++) {
-
-    fprintf(f_time, "%s,", experimentID);
-    fprintf(f_time, "%d,", rank);
-    fprintf(f_time, "%lu,", final);
-    fprintf(f_time, "%s,", distribution);
-    fprintf(f_time, "%f,", a);
-    fprintf(f_time, "%f,", b);
-    fprintf(f_time, "%u,", iterations);
-    fprintf(f_time, "%lu,%lu,%lu,%lu,%lu",
-        times_buffer[i].sleep         ,
-        times_buffer[i].start         ,
-        times_buffer[i].end           ,
-        times_buffer[i].expected_sleep,
-        times_buffer[i].start - times_buffer[i].sleep);
-    fprintf(f_time, "\n");
-  }
-  fprintf(f_time, "\n");
-#else
-  app_end_time   = 1000000000 * MPI_Wtime();
-  app_start_time = 1000000000 * app_start_time;
-  fprintf(f_time, "%lu,",
-         (unsigned long) (app_end_time - app_start_time) );
-  fprintf(f_time, "\n");
-#endif
-  printf("%lu", (unsigned long) (app_end_time - app_start_time));
-      //char[1024]
-//    }
   free(r);
-  fclose(f_time);
+}
 
-#ifdef USE_METRICS
-  //fclose(f);
-#endif
+void print_buffer(double a, double b, char * distribution, 
+		  int iterations, struct coll_time * times_buffer, double cpn)
+{
+	const char *str = "0123456789abcdef";
+	char tmpfile[512];
+	char tmpbuff[64];
+	char hostname[1024];
+	char out[20];
+	char experimentID[20];
+  	FILE *f_time;
+	int rank;
+  	int root = 0;
+  	struct stat st = {0};
+  	int i, length = 13;
 
-  return 0;
+	hostname[1023] = '\0';
+	gethostname(hostname, 1023);
+	// Create new directories in tmp: /tmp/results/<DISTRIBUTION> to
+	// store results before writing back to the file system.
+	strcpy(tmpfile, "/tmp/results");
+	if (stat(tmpfile, &st) == -1) {
+		mkdir(tmpfile, 0700);
+	}
+	strcat(tmpfile, "/");
+	strcat(tmpfile, hostname);
+	strcat(tmpfile, "_");
+
+	srand((unsigned int) time(0) + getpid());
+
+	for (i = 0; i < length; i++) {
+		out[i] = str[(rand() % 16)];
+		srand(rand());
+	}
+  	out[i] = 0;
+  	strcat(tmpfile, out);
+  	strcat(tmpfile, ".csv");
+
+  	f_time = fopen(tmpfile, "a");
+
+	/* Share the experiment ID across ranks */
+  	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  	if (rank == 0) {
+    		srand((unsigned int) time(0) + getpid());
+    		for (i = 0; i < length; i++) {
+      			experimentID[i] = str[(rand() % 16)];
+      			srand(rand());
+    		}
+    		experimentID[i] = 0;
+  	}
+  	MPI_Bcast(&experimentID, sizeof(experimentID),
+                  MPI_CHAR, root, MPI_COMM_WORLD);
+
+	/* Print the logged data to the local data file */
+  	for (i = 0; i < iterations; i++) {
+		fprintf(f_time, "%s,", experimentID);
+    		fprintf(f_time, "%d,", rank);
+   		fprintf(f_time, "%lu,", 0);
+    		fprintf(f_time, "%s,", distribution);
+    		fprintf(f_time, "%f,", a);
+    		fprintf(f_time, "%f,", b);
+    		fprintf(f_time, "%u,", iterations);
+    		fprintf(f_time, "%lu,%lu,%lu,%lu,%lu",
+        			times_buffer[i].sleep         ,
+        			times_buffer[i].start         ,
+        			times_buffer[i].end           ,
+        			times_buffer[i].expected_sleep,
+        			times_buffer[i].start - times_buffer[i].sleep);
+    		fprintf(f_time, "\n");
+  	}
+  	fprintf(f_time, "\n");
+  	fclose(f_time);
 }
 
 
-int
-main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 
-  char distribution[20] = "";
-  double inter_mean = 0;
-  double inter_sttdev = 0;
-  int iterations = 0;
-  int rank, nprocs;
+	char distribution[20] = "";
+	double a, b;
+	int iterations = 0;
+	int rank, nprocs;
 
-  if(argc < 5){
-    printf("Usage: app_gen <a> <b> <computation distribution> <iterations> \n");
-    return -1;
-  }
+	if(argc != 5){
+    		printf("Usage: %s <a> <b> <computation distribution> <iterations> \n", argv[0]);
+    		return -1;
+  	}
 
-  MPI_Init(&argc,&argv);
-  MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
-  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+	MPI_Init(&argc,&argv);
+  	MPI_Comm_size(MPI_COMM_WORLD,&nprocs);
+  	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
-  sscanf(argv[1], "%lf", &inter_mean);
-  sscanf(argv[2], "%lf", &inter_sttdev);
-  sscanf(argv[3], "%s", distribution);
-  iterations = atoi(argv[4]);
+  	sscanf(argv[1], "%lf", &a);
+  	sscanf(argv[2], "%lf", &b);
+  	sscanf(argv[3], "%s", distribution);
+  	iterations = atoi(argv[4]);
 
-  struct coll_time times_buffer[ iterations ];
-  double cpn = get_clocks_per_nanosecond();
+  	double cpn = get_clocks_per_nanosecond();
+  	struct coll_time *times_buffer = (struct coll_time *)calloc(iterations, sizeof(struct coll_time));
 
-  barrier_loop(inter_mean, inter_sttdev, distribution, iterations, times_buffer, cpn);
+  	barrier_loop(a, b, distribution, iterations, times_buffer, cpn);
+  	print_buffer(a, b, distribution, iterations, times_buffer, cpn);
+	free(times_buffer);
 
-  MPI_Finalize();
-
-  return 0;
-
+  	MPI_Finalize();
 }
