6af053a427abb73bc1b0a049cd88fefd5e61d968
Patrick G. Bridges <patrickb@unm.edu>
2019-10-15 15:31:55 -0600

    Fixed commands.sh

--- commands.sh
+++ commands.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p /tmp/results/
+export HOST=`hostname`
 export OUTFILE=`mktemp /tmp/results/${HOST}_XXXXXXXX.csv`
 /home/docker/bsp_prototype ${OUTFILE} "$@"
 mv ${OUTFILE} /results/
