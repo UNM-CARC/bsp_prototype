dfe7a23f5344a6b125cfec9aba27f5ee1bdba3af
Quincy Wofford <quincy.wofford@gmail.com>
2019-11-14 16:10:16 -0700

    add ldms launch and cleanup

--- commands.sh
+++ commands.sh
@@ -11,6 +11,12 @@ export LDMS_MPI_PROFILER_LIB_NAME=libldms_mpi_profiler.so
 export LDMS_MPI_PROFILER_PATH=$LDMS_INSTALL_PATH/lib/ovis-ldms/$LDMS_MPI_PROFILER_LIB_NAME
 export LD_PRELOAD=/usr/lib64/openmpi3/lib/libmpi.so:$LDMS_MPI_PROFILER_PATH
 
+# Launch ldms sampler
+/opt/ldms_wheeler/start_sampler.sh
+# Launch workload
 /home/docker/bsp_prototype "$@" ${OUTFILE}
+# Clean up results
 mv ${OUTFILE} /results/
+# Kill ldms sampler
+kill $(ps aux | grep ldms | grep -v grep | awk '{print $2}')
 exit "$?"
