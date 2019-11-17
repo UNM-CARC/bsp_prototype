353fdff45d95d5d6d3fea3e250ffc07795b68743
qwofford <qwofford@datamunger.ddns.net>
2019-11-17 15:46:32 -0700

    adding env vars needed for mpi sampling

--- commands.sh
+++ commands.sh
@@ -12,9 +12,12 @@ export LDMS_MPI_PROFILER_PATH=$LDMS_INSTALL_PATH/lib/ovis-ldms/$LDMS_MPI_PROFILE
 export LD_PRELOAD=/usr/local/lib/libmpi.so:$LDMS_MPI_PROFILER_PATH
 # Choose mpi functions to sample
 export LDMS_SHM_MPI_FUNC_INCLUDE="MPI_Send,MPI_Recv,MPI_Barrier,MPI_Isend,MPI_Irecv,MPI_Wait"
+export LDMS_SHM_MPI_PROFILER_LOG_LEVEL=1
+# Name of the shared index. This should be the same in the mpi sampler configuration (shm_index=...).
+export LDMS_SHM_INDEX="/ldms_shm_mpi_index"
 
 # Launch workload
-mpirun -n $PBS_NP -mca pml ucx --mca btl ^vader,tcp,openib,uct -x UCX_NET_DEVICES=mlx4_0:1 -x MPI_RUN_COMMAND -x PBS_O_HOST -x LDMS_SHM_MPI_FUNC_INCLUDE -x PBS_JOBNAME -x LDMS_MPI_PROFILER_PATH -x LDMS_SHM_INDEX -x LDMS_SHM_MPI_PROFILER_LOG_LEVEL -x LD_PRELOAD_CONTAINER -machinefile $PBS_NODEFILE /home/docker/bsp_prototype "$@" ${OUTFILE}
+mpirun -n $PBS_NP -mca pml ucx --mca btl ^vader,tcp,openib,uct -x UCX_NET_DEVICES=mlx4_0:1 -x PBS_O_HOST -x LDMS_SHM_MPI_FUNC_INCLUDE -x PBS_JOBNAME -x LDMS_MPI_PROFILER_PATH -x LDMS_SHM_INDEX -x LDMS_SHM_MPI_PROFILER_LOG_LEVEL -x LD_PRELOAD -machinefile $PBS_NODEFILE /home/docker/bsp_prototype "$@" ${OUTFILE}
 # Clean up results
 mv ${OUTFILE} /results/
 exit "$?"
