f6ae9f2effac3e6cbd53a4c67fc38bea45c0fdb0
qwofford <qwofford@datamunger.ddns.net>
2019-11-19 15:43:13 -0700

    need to have preload library in path before libmpi

--- commands.sh
+++ commands.sh
@@ -9,7 +9,7 @@ export OUTFILE=`mktemp /tmp/results/${HOST}_XXXXXXXX.csv`
 export LDMS_INSTALL_PATH=/usr/local
 export LDMS_MPI_PROFILER_LIB_NAME=libldms_mpi_profiler.so
 export LDMS_MPI_PROFILER_PATH=$LDMS_INSTALL_PATH/lib/ovis-ldms/$LDMS_MPI_PROFILER_LIB_NAME
-export LD_PRELOAD=/usr/local/lib/libmpi.so:$LDMS_MPI_PROFILER_PATH
+export LD_PRELOAD=$LDMS_MPI_PROFILER_PATH:/usr/local/lib/libmpi.so:$LD_PRELOAD
 # Choose mpi functions to sample
 export LDMS_SHM_MPI_FUNC_INCLUDE="MPI_Send,MPI_Recv,MPI_Barrier,MPI_Isend,MPI_Irecv,MPI_Wait"
 export LDMS_SHM_MPI_PROFILER_LOG_LEVEL=1
@@ -17,6 +17,7 @@ export LDMS_SHM_MPI_PROFILER_LOG_LEVEL=1
 export LDMS_SHM_INDEX="/ldms_shm_mpi_index"
 
 # Launch workload
+set -x
 mpirun -n $PBS_NP -mca pml ucx --mca btl ^vader,tcp,openib,uct -x UCX_NET_DEVICES=mlx4_0:1 -x PBS_O_HOST -x LDMS_SHM_MPI_FUNC_INCLUDE -x PBS_JOBNAME -x LDMS_MPI_PROFILER_PATH -x LDMS_SHM_INDEX -x LDMS_SHM_MPI_PROFILER_LOG_LEVEL -x LD_PRELOAD -machinefile $PBS_NODEFILE /home/docker/bsp_prototype "$@" ${OUTFILE}
 # Clean up results
 mv ${OUTFILE} /results/
