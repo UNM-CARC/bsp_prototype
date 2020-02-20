da100e5c7badb964527f13b196bca86cf8d21e60
Quincy Wofford <quincy.wofford@gmail.com>
2020-02-20 08:47:55 -0700

    adding mods to make bsp run over ldms image layer

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,16 @@
 # set the base image used for building can control this by setting the 
 # BASE_IMAGE variable instead.
 
-ARG DOCKER_TAG=latest
-ARG BASE_IMAGE=unmcarc/docker_base:${DOCKER_TAG}
+ARG DOCKER_TAG=carc-wheeler
+ARG BASE_IMAGE=qwofford/docker_ldms:${DOCKER_TAG}
 FROM ${BASE_IMAGE}
+# Because we use spack and a cleaned environment, the run commands here
+# need to be login shells to get the appropriate spack initialiation.
+SHELL ["/bin/bash", "-l", "-c"]
+RUN spack env activate -d /home/docker
+
+# All commands will run relative to this WORKDIR
+WORKDIR /home/docker
 
 # We force our current prototype of the sprng package into the spack
 # repo on this container to try it out for now. Once it's added to an
@@ -15,23 +22,29 @@ FROM ${BASE_IMAGE}
 RUN spack create --skip-editor -n sprng \
     && rm /opt/spack/var/spack/repos/builtin/packages/sprng/package.py
 COPY sprng.py /opt/spack/var/spack/repos/builtin/packages/sprng/package.py
+
 # Add the new packages we want to the environment and regenerate its
 # view in /usr/local
-WORKDIR /home/docker
-COPY Makefile bsp_prototype.c gsl-sprng.h /home/docker/
-RUN spack add sprng gsl openblas osu-micro-benchmarks\
+RUN spack add sprng gsl \
     && spack concretize \
     && spack install \
     && spack env view regenerate \
     && spack clean -a
 
-# Finally, compile any addtional programs needed locally and set our command
-# that will be run on container start.
 
-#--user did not work for pip install!
-RUN yum install -y rabbitmq-server.noarch python3\
-    && pip3 install pika --upgrade\ 
-    && echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config
+COPY Makefile bsp_prototype.c gsl-sprng.h /home/docker/
+RUN spack add sprng gsl openblas osu-micro-benchmarks \
+    && spack concretize \
+    && spack install \
+    && spack env view regenerate \
+    && spack clean -a
+
+# Ask Sahba
+RUN yum install -y rabbitmq-server.noarch python3 \
+    && pip3 install pika --upgrade \ 
+    && echo "[{rabbit, [{loopback_users, []}]}]." \
+      > /etc/rabbitmq/rabbitmq.config
+
 COPY commands.sh /home/docker/commands.sh
 RUN make bsp_prototype
 COPY rabit_functions.py /home/docker/rabit_functions.py
--- commands.sh
+++ commands.sh
@@ -1,32 +1,61 @@
 #!/bin/bash
-mkdir -p /tmp/results/
-export PATH=$PATH:/opt/software/linux-centos7-x86_64/gcc-8.3.1/osu-micro-benchmarks-5.6.1-gxlojmj2kwdr366txez2ko6tw75yi4o3/libexec/osu-micro-benchmarks/mpi/pt2pt
-export PYTHONPATH="/root/.local/lib/python3.6/site-packages":$PYTHONPATH
-export HOST=`hostname`
-RABBIT_SCRIPT="/home/docker/rabit_functions.py"
-if [[ $1 == "osu" ]] 
-then
-	#passing all but the first arg to osu_bw
-	osu_bw "${@:2:$#}"
-	exit "$?"
 
-elif [[ $1 == 'rabbit' ]]
-then
-	PYTHON="/usr/bin/python3"
-	export PYTHONPATH=/opt/bsp_prototype/pip_files/lib/python3.6/site-packages:$PYTHONPATH
-	if [[ $2 == 'server' ]]
-	then
-    	rabbitmq-server
-	else
-		$PYTHON $RABBIT_SCRIPT "${@:2:$#}"
-	fi
-	exit "$?"
+source /home/docker/env.sh
 
-fi
-export OUTFILE=`mktemp /tmp/results/${HOST}_XXXXXXXX.json`
-rm ${OUTFILE}	# Let the bsp_prototype actually crate hte file if it wants it.
-/home/docker/bsp_prototype "$@" ${OUTFILE}
-if [ -f ${OUTFILE} ]; then
-	mv ${OUTFILE} /results/
-fi
+# Establish filename suffix
+export OUTFILE="${APPLICATION_OUTPUT_DIR}/$RANDOM.csv"
+
+#########################################
+#
+#  Setup LDMS envinroment
+#
+#########################################
+
+# LDMS environment variables for openmpi sampling
+# NOTE: If you aren't using OpenMPI, change LD_PRELOAD.
+export LD_LIBRARY_PATH=/usr/local/lib/ovis-lib:/usr/local/lib/ovis-ldms:${LD_LIBRARY_PATH}
+export LDMS_INSTALL_PATH=/usr/local
+export LDMS_MPI_PROFILER_LIB_NAME=libldms_mpi_profiler.so
+export LDMS_MPI_PROFILER_PATH=$LDMS_INSTALL_PATH/lib/ovis-ldms/$LDMS_MPI_PROFILER_LIB_NAME
+export LD_PRELOAD=$LDMS_MPI_PROFILER_PATH:/usr/local/lib/libmpi.so:$LD_PRELOAD
+# Choose mpi functions to sample
+export LDMS_SHM_MPI_FUNC_INCLUDE="MPI_Send,MPI_Recv,MPI_Barrier,MPI_Isend,MPI_Irecv,MPI_Wait"
+export LDMS_SHM_MPI_PROFILER_LOG_LEVEL=1
+# Name of the shared index. This should be the same in the mpi sampler configuration (shm_index=...).
+export LDMS_SHM_INDEX="/ldms_shm_mpi_index"
+
+#########################################
+
+# Launch the LDMS sampler on all nodes
+echo STARTING SAMPLERS
+for i in $(cat $PBS_NODEFILE | uniq); do
+  ssh $i "source /etc/profile; module load openssl-1.1.1b-gcc-4.8.5-obdqvnl; module load singularity-3.2.1-gcc-4.8.5-ulix7vo; singularity exec -B /etc/hostname -B ${OUTPUT_DIR} ${CONTAINER_IMAGE_DIR}/bsp_prototype /opt/ldms_wheeler/start_sampler.sh"
+done;
+echo SAMPLERS SHOULD HAVE STARTED
+
+########################################
+#
+# Layer 2 Container environment setup
+#
+########################################
+
+# Same as above but with part of the environment shoved into the command
+export OMPI_MCA_orte_launch_agent="${CONTAINER_IMAGE_DIR}/bsp_prototype/home/docker/ompi_launch.sh ${CONTAINER_IMAGE_DIR}"
+
+
+########################################
+#
+# Launch the application in L2 context
+#
+########################################
+
+mpirun -mca pml ucx -mca btl ^vader,tcp,openib,uct -x UCX_NET_DEVICES=mlx4_0:1 -n $PBS_NP -x PBS_O_HOST -x LDMS_SHM_MPI_FUNC_INCLUDE -x PBS_JOBNAME -x LDMS_MPI_PROFILER_PATH -x LDMS_SHM_INDEX -x LDMS_SHM_MPI_PROFILER_LOG_LEVEL -x LD_PRELOAD -machinefile $PBS_NODEFILE /home/docker/bsp_prototype "$@" ${OUTFILE}
+
+# Kill LDMS sampler on all nodes
+
+for i in $(cat $PBS_NODEFILE | uniq); do
+  echo ssh $i "kill \$(ps aux | grep ldmsd | grep -v grep | awk "\'"{print \$2}"\'")"
+  ssh $i "kill \$(ps aux | grep ldmsd | grep -v grep | awk "\'"{print \$2}"\'")"
+done;
+
 exit "$?"
