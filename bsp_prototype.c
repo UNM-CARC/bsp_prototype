bf3523778a084200dc0d171a806cdfe1b3be86ab
Quincy Wofford <quincy.wofford@gmail.com>
2020-02-21 16:39:27 -0700

    corrected scoping problem, made default rng a gaussian

--- bsp_prototype.c
+++ bsp_prototype.c
@@ -169,7 +169,7 @@ enum rng_type init_rng_type(char *distribution)
 		return RNG_PARETO;
     	} else if (strcmp(distribution, "constant") == 0)
 		return RNG_CONSTANT;
-	else return RNG_ERROR;
+	else return RNG_GAUSSIAN;
 }
 
 double generate_interval_rng(gsl_rng *r, enum rng_type rng_type, double a, double b) 
@@ -302,7 +302,7 @@ void fill(double *p, int n)
 int init_workload(int w, gsl_rng *r, char *distribution, double a, double b)
 {
 	double *buf;
-  	enum rng_type rng_type = init_rng_type(distribution);
+  	rng_type = init_rng_type(distribution);
 	switch (w) {
 	case WORKLOAD_SLEEP:
   		if (rng_type < 0) {
