8fd6f1290d48c4ba52aa99fd1e1e403ad588eec0
Quincy Wofford <quincy.wofford@gmail.com>
2020-02-20 17:03:10 -0700

    off by one index error passing args to orted

--- ompi_launch.sh
+++ ompi_launch.sh
@@ -9,6 +9,6 @@ source /etc/profile;
 module load openssl-1.1.1b-gcc-4.8.5-obdqvnl; 
 module load singularity-3.2.1-gcc-4.8.5-ulix7vo; 
 
-singularity run -B /etc/hostname -B ${CONTAINER_IMAGE_DIR} ${CONTAINER_IMAGE_DIR}/bsp_prototype orted "${@:1:$#}"
+singularity run -B /etc/hostname -B ${CONTAINER_IMAGE_DIR} ${CONTAINER_IMAGE_DIR}/bsp_prototype orted "${@:2:$#}"
 
 echo ======= END: $(hostname) ompi_launch.sh=======
