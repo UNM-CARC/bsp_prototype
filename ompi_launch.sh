945b150e6522d126742e40527e4615170cc24fdb
Quincy Wofford <quincy.wofford@gmail.com>
2020-03-05 09:13:06 -0700

    changing from wheeler to stampede2 relevant paths and modules

--- ompi_launch.sh
+++ ompi_launch.sh
@@ -1,12 +1,11 @@
 echo ======= BEGIN: $(hostname) ompi_launch.sh=======
 
 echo Beginning ompi_launch.sh
-source /etc/profile; 
+source /etc/profile;
 
-module load openssl-1.1.1b-gcc-4.8.5-obdqvnl; 
-module load singularity-3.2.1-gcc-4.8.5-ulix7vo; 
+module load tacc-singularity/3.4.2
 
 #singularity run -B /etc/hostname -B ${CONTAINER_IMAGE_DIR} ${CONTAINER_IMAGE_DIR}/bsp_prototype orted $@
-singularity run -B /etc/hostname -B /wheeler/scratch/$(whoami) /wheeler/scratch/$(whoami)/bsp_prototype orted $@
+singularity run -B /etc/hostname -B ${SCRATCH}/$(whoami) ${SCRATCH}/$(whoami)/bsp_prototype orted $@
 
 echo ======= END: $(hostname) ompi_launch.sh=======
